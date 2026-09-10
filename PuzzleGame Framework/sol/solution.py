dd8fe3ec395caafbf346f22af5f361758307df7f
Lucas Swearingen <swearingenl1@udayton.edu>
2026-09-09 23:26:15 -0400

    Cleaned up unecessary folders

---
+++ PuzzleGame Framework/sol/solution.py
@@ -0,0 +1,31 @@
+#This is the only file you need to work on. You do NOT need to modify other files
+
+# Below are the functions you need to implement. For the first project, you only need to finish implementing iddfs() 
+# ie iterative deepening depth first search
+
+
+# here you need to implement the Iterative Deepening Search Method
+def iterativeDeepening(puzzle):
+
+    # Find "8" position
+
+    # Successor function - possible child states
+    list = []
+    print('Iterative Deepening')
+
+    # Return the path
+    return list
+
+
+# This will be for next project
+def astar(puzzle):
+    list = []
+    return list
+
+
+
+
+
+
+
+
